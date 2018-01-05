ac2441fa314ff0010d7e9e553657e12c0ae4daef
Bar Schwartz <sch.bar@gmail.com>
2018-01-05 15:45:22 +0200

    login screen

--- app/src/main/java/il/ac/tau/cloudweb17a/hasorkimmanagers/LogInActivity.java
+++ app/src/main/java/il/ac/tau/cloudweb17a/hasorkimmanagers/LogInActivity.java
@@ -1,22 +1,109 @@
 package il.ac.tau.cloudweb17a.hasorkimmanagers;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.annotation.StringRes;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GetTokenResult;
+
+import java.util.Collections;
+
 public class LogInActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    View mRootView;
+    private static final String TAG = LogInActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        mRootView = findViewById(R.id.root);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            auth.getCurrentUser().getIdToken(true).addOnSuccessListener(new OnSuccessListener<GetTokenResult>() {
+                @Override
+                public void onSuccess(GetTokenResult result) {
+                    String idToken = result.getToken();
+                    //Do whatever
+                    GoToReportList(idToken);
+                }
+            });
+
+        } else {
+            // not signed in
+            Bundle params = new Bundle();
+            params.putString(AuthUI.EXTRA_DEFAULT_COUNTRY_CODE, "il");
+
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(
+                                    Collections.singletonList(new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).setParams(params).build()))
+                            .setIsSmartLockEnabled(false)
+                            .build(),
+                    RC_SIGN_IN);
+
+        }
     }
 
 
-    public void GoToReportList(View view) {
+    public void GoToReportList(String idToken) {
         Intent intent = new Intent(LogInActivity.this, ReportListActivity.class);
+        intent.putExtra("token", idToken);
         startActivity(intent);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                GoToReportList(response.getIdpToken());
+                finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar(R.string.unknown_error);
+                    return;
+                }
+            }
+
+            Log.e(TAG, getString(R.string.unknown_sign_in_response));
+        }
+    }
+
+    @MainThread
+    private void showSnackbar(@StringRes int errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
     }
 
 }
