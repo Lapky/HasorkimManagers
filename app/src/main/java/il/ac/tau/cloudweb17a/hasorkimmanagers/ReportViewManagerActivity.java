4866b3a4ec29a9fb9f9f4460f8298e11b75cb3d2
Shahar <shaharman5@gmail.com>
2018-01-07 04:35:23 +0200

    commenting out mDrawer

--- app/src/main/java/il/ac/tau/cloudweb17a/hasorkimmanagers/ReportViewManagerActivity.java
+++ app/src/main/java/il/ac/tau/cloudweb17a/hasorkimmanagers/ReportViewManagerActivity.java
@@ -1,37 +1,33 @@
 package il.ac.tau.cloudweb17a.hasorkimmanagers;
 
-        import android.content.Intent;
-        import android.graphics.Bitmap;
-        import android.graphics.Color;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.LayoutInflater;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.AdapterView;
-        import android.widget.Button;
-        import android.widget.ImageView;
-        import android.widget.LinearLayout;
-        import android.widget.ListView;
-        import android.widget.PopupWindow;
-        import android.widget.TextView;
-
-        import com.google.firebase.database.DataSnapshot;
-        import com.google.firebase.database.DatabaseError;
-        import com.google.firebase.database.DatabaseReference;
-        import com.google.firebase.database.FirebaseDatabase;
-        import com.google.firebase.database.ValueEventListener;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
-        import java.util.ArrayList;
-        import java.util.Objects;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ReportViewManagerActivity extends AppCompatActivity {
 
+    //private DrawerLayout mDrawer;
     private LayoutInflater layoutInflater;
     private ViewGroup thisContainer;
     private PopupWindow popupWindow;
@@ -54,7 +50,7 @@ public class ReportViewManagerActivity extends AppCompatActivity {
 
         thisContainer = (ViewGroup) layoutInflater.inflate(R.layout.activity_report_view_manager, null);
 
-        mDrawer.addView(thisContainer, 0);
+        //mDrawer.addView(thisContainer, 0);
 
         report = (Report) getIntent().getSerializableExtra("Report");
         isManager = (Boolean) getIntent().getSerializableExtra("isManager");
@@ -103,12 +99,13 @@ public class ReportViewManagerActivity extends AppCompatActivity {
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot messageSnapshot: dataSnapshot.getChildren()) {
+                for (DataSnapshot messageSnapshot : dataSnapshot.getChildren()) {
 
                     String userId = messageSnapshot.getKey();
                     String duration = messageSnapshot.getValue().toString();
                     boolean isAssignedScanner = false;
-                    if (Objects.equals(userId, report.getAssignedScanner())) isAssignedScanner = true;
+                    if (Objects.equals(userId, report.getAssignedScanner()))
+                        isAssignedScanner = true;
 
                     scannerList.add(new Scanner(userId, duration, isAssignedScanner));
                 }
@@ -122,6 +119,7 @@ public class ReportViewManagerActivity extends AppCompatActivity {
                 listView.setAdapter(adapter);
 
             }
+
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
@@ -129,32 +127,22 @@ public class ReportViewManagerActivity extends AppCompatActivity {
         });
     }
 
-    public void sendScannerHandler(View view)
-    {
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        startActivity(new Intent(ReportViewManagerActivity.this, ReportListActivity.class));
-        finish();
-
-        LinearLayout vwParentRow = (LinearLayout)view.getParent();
-        TextView scanner_name = (TextView)vwParentRow.getChildAt(2);
+    public void sendScannerHandler(View view) {
+        LinearLayout vwParentRow = (LinearLayout) view.getParent();
+        TextView scanner_name = (TextView) vwParentRow.getChildAt(2);
 
         String scannerNameString = scanner_name.getText().toString();
 
-        Button sendScanner = (Button)vwParentRow.getChildAt(3);
+        Button sendScanner = (Button) vwParentRow.getChildAt(3);
 
         String assignedScanner = report.getAssignedScanner();
 
         if (assignedScanner != null && !Objects.equals(assignedScanner, "") &&
-                !Objects.equals(assignedScanner, scannerNameString)){
+                !Objects.equals(assignedScanner, scannerNameString)) {
             sendScanner.setError("יכול להיות רק סורק מאושר אחד");
-        }
-        else{
+        } else {
 
-            if (!Objects.equals(report.getAssignedScanner(), scannerNameString)){
+            if (!Objects.equals(report.getAssignedScanner(), scannerNameString)) {
 
                 report.reportUpdateAssignedScanner(scannerNameString);
 
@@ -163,8 +151,7 @@ public class ReportViewManagerActivity extends AppCompatActivity {
                 vwParentRow.setBackgroundColor(Color.YELLOW);
 
                 report.reportUpdateStatus("MANAGER_ASSIGNED_SCANNER");
-            }
-            else{
+            } else {
                 report.reportUpdateAssignedScanner("");
                 sendScanner.setText("שלח");
                 vwParentRow.setBackgroundColor(Color.WHITE);
@@ -177,6 +164,13 @@ public class ReportViewManagerActivity extends AppCompatActivity {
 
 
         //Log.d(TAG, scanner_name.getText().toString());
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        startActivity(new Intent(ReportViewManagerActivity.this, ReportListActivity.class));
+        finish();
     }
 }
 
